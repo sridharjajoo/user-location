4ab397323f0d2b4b92ada9ebad375eaad6142346
jajoosridhar <sridhar.jajoo@oyorooms.com>
2020-03-27 12:33:55 +0530

    added polylinefill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,11 +44,33 @@ export class AppComponent {
 
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
     let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    let marker = new google.maps.Marker({
-      position: location,
-      map: this.map,
-      title: 'Got you!'
+    
+    var flightPlanCoordinates = [
+      {lat: 28.3991744, lng: 77.0365766},
+      {lat: position.coords.latitude, lng: position.coords.longitude},
+      {lat: 28.74939, lng: 77.1023706},
+      {lat: 28.699581, lng: 77.2549423}
+    ];
+
+    let marker;
+    for (let i = 0; i < flightPlanCoordinates.length; i++) {
+        location = new google.maps.LatLng(flightPlanCoordinates[i].lat, flightPlanCoordinates[i].lng);
+        marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+
+    var flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
     });
+
+    flightPath.setMap(this.map);
 
     marker.addListener('click', this.simpleMarkerHandler);
 
